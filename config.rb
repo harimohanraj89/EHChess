65c22343474025a40ea8a1ad30e7f54e90b87675
Hariharan Mohanraj <hari.mohanraj89@gmail.com>
2014-01-27 19:31:36 -0500

    more tweaks

--- config.rb
+++ config.rb
@@ -4,5 +4,7 @@ ActiveRecord::Base.establish_connection(
 		:encoding => 'unicode',
 		:adapter => 'postgresql',
 		:database => 'dea08e7geo1k7',
-		:host => 'ec2-54-225-101-164.compute-1.amazonaws.com'
+		:host => 'ec2-54-225-101-164.compute-1.amazonaws.com',
+		:username => 'lbzbfllvaavjne',
+		:password => 'h5CF4YJKD1he0ZgTcMy29X6IL-'
 )
